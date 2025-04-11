a90d3ae80b742b0a27d2300fc35d359d363e9758
zrn2003 <zishanrn2003@gmail.com>
2025-04-11 09:39:34 +0530

    main

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -25,7 +25,7 @@ router.post('/add', async (req, res) => {
         res.json({
             status: true,
             message: "Product added successfully",
-            product: productObj
+            
         });
 
     } catch (error) {
@@ -41,7 +41,7 @@ router.get('/get', async (req, res) => {
         const products = await Product.find();
         res.json({
             status: true,
-            products
+
         });
     } catch (error) {
         res.json({
@@ -50,5 +50,40 @@ router.get('/get', async (req, res) => {
         });
     }
 });
+
+router.delete('/delete/:id',async(req,res)=>{
+    // findByIdAndDelete
+    try{
+        const id = req.params.id
+        await Product.findByIdAndDelete(id)
+        res.json({
+            status:true,
+            message:'Product deleted successfully !!'
+        })
+
+    }catch(err){
+        res.json({
+            status:false,
+            message:err
+        })
+    }
+})
+
+router.put('/update/:id',async(req,res)=>{
+    try{
+        const id = req.params.id
+        // findByIdAndUpdate(id,body,{new:true})
+        const updated = await Product.findByIdAndUpdate(id,req.body,{'new':true})
+        res.json({
+            status:true,
+            message:'Product updated successfully !!'
+        })
+    }catch(err){
+        res.json({
+            status:false,
+            message:err
+        })
+    }
+})
 
 module.exports = router;
